b7b8670fe8f1a28db65cb48717ef76a9d76e100f
anndjella <stankovicandjela53@gmail.com>
2025-08-28 12:20:19 +0200

    Start implementing /api/predict

--- src/projekat/server.clj
+++ src/projekat/server.clj
@@ -1,6 +1,9 @@
 (ns projekat.server
   (:require [clojure.edn :as edn]
-            [projekat.lm :as lm]))
+            [projekat.lm :as lm]
+            [ring.util.response :as resp]
+            [ring.adapter.jetty :as jetty]
+            [cheshire.core :as json]))
 
 (defn load-artifact []
   (edn/read-string (slurp "resources/lm-artifact.edn")))
@@ -15,3 +18,30 @@
         sum-bx (reduce + (map * betas xs))
         rating   (+ (double intercept) sum-bx)]
     {:prediction     rating}))
+
+(defn json-response
+  ([m] (json-response 200 m))
+  ([status m]
+   (-> (resp/response (json/generate-string m))
+       (resp/status status)
+       (resp/header "Content-Type" "application/json; charset=utf-8"))))
+
+(defn handler [req]
+  (let [{:keys [request-method uri]} req]
+    (cond 
+      (and (= request-method :post) (= uri "/api/predict"))
+      (let [raw  (slurp (:body req))
+            body (json/parse-string raw true)]
+        (json-response {:received body}))
+
+      :else
+      (-> (resp/response "Not found")
+          (resp/status 404)
+          (resp/header "Content-Type" "text/plain; charset=utf-8")))))
+
+
+(def app handler)
+
+(defn -main [& _]
+  (println "Backend on http://localhost:3000")
+  (jetty/run-jetty app {:port 3000 :join? false}))
